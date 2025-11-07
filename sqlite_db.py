3f20ef0763dfe838e738a948d7e02a3cdb77ec70
LebohangMashinini <palesal613@gmail.com>
2025-11-08 01:03:19 +0200

    sqlite_db only handles table creation

--- sqlite_db.py
+++ sqlite_db.py
@@ -1,24 +1,20 @@
 import sqlite3
 
-conn = sqlite3.connect('seed_data.db')
-cur = conn.cursor()
+def create_db():
+    conn = sqlite3.connect('data.db')
+    cur = conn.cursor()
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        date_of_birth TEXT,
+        email TEXT,
+        city TEXT 
+    )
+    ''')
+    conn.commit()
+    conn.close()
+    print("Database and table created.")
 
-cur.execute('''
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    date_of_birth TEXT,
-    email TEXT,
-    city TEXT 
-)
-''')
-conn.commit()
-
-conn = sqlite3.connect('data.db')
-cur = conn.cursor()
-cur.execute('SELECT * FROM users LIMIT 10')
-for row in cur.fetchall():
-    print(row)
-
-conn.close()
-
+if __name__ == "__main__":
+    create_db()
